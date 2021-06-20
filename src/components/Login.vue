0d31059c5d559197c08de5ef4c0097c2a3dac295
zzw <zzwpublic@163.com>
2021-06-20 11:13:01 +0800

    commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,102 @@
 <template>
-  <div>
-    登陆组件
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" />
+      </div>
+      <el-form ref="loginFormRef" :model="loginForm" :rules="rules" label-width="0px" class="login_form">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-suo" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登陆</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data () {
+      return {
+        loginForm: {
+          username: 'admin',
+          password: '123456'
+        },
+        rules: {
+          username: [
+            { required: true, message: '请输入活动名称', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      resetLoginForm () {
+        this.$refs.loginFormRef.resetFields()
+      },
+      login () {
+          this.$refs.loginFormRef.validate(async valid => {
+            if (!valid) return
+            const { data: res } = await this.$http.post('login', this.loginForm)
+            if (res.meta.status !== 200) return this.$message.error('Login Failed')
+            window.sessionStorage.setItem('token', res.data.token)
+            this.$router.push('/home')
+        })
+      }
+    }
+  }
+
 </script>
 
 <style lang="less" scoped>
+  .login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+  }
+
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .avatar_box {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+import './assets/css/global.css'
 
 Vue.use(Router)
 
-export default new Router({
+ const router = new Router({
   routes: [
+    // 重定向
     { path: '/', redirect: '/login' },
-  { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
   ]
 })
+// 路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const token = window.sessionStorage.getItem('token')
+  if (!token) return next('/login')
+  next()
+})
+
+export default router
