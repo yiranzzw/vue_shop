9466ad34e9c10149d161932d9cbb0255440f1819
zzw <zzwpublic@163.com>
2021-07-11 21:00:43 +0800

    add files

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -30,7 +30,7 @@
         },
         rules: {
           username: [
-            { required: true, message: '请输入活动名称', trigger: 'blur' },
+            { required: true, message: '请输入用户名', trigger: 'blur' },
             { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
           ]
         }
